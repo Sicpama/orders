37a6b6eada393ef3ac1c11081451d731e39f9b42
Chris Hewison <hewisonchris@gmail.com>
2025-05-15 13:41:07 +0900

    chore: handle Linux also for connect

--- 3_connect.sh
+++ 3_connect.sh
@@ -1,1 +1,14 @@
-docker exec -it orders-db mysql -u dev --password=pwd -e "use orders; show tables; select count(*) from orders;"
+# Detect the operating system
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS
+    HOST="localhost"
+elif [[ "$(uname)" == "Linux" ]]; then
+    # Ubuntu/Linux
+    HOST="127.0.0.1"
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+
+# Use the determined host in the docker exec command
+docker exec -it orders-db mysql -h $HOST -u dev --password=pwd -e "use orders; show tables; select count(*) from orders;"
